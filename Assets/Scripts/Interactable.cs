7db92ff42966180d30194a20854d79ab31da9753
AdamPlett <72412169+AdamPlett@users.noreply.github.com>
2026-09-18 15:35:23 -0500

    Work on interactable class and laneblocker placeholder sprite

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -2,8 +2,21 @@ using UnityEngine;
 
 public abstract class Interactable : MonoBehaviour
 {
+    [Header("Stats")]
+    public float damage = .25f;
+
+    [Header("Setup")]
     public Sprite[] spriteArray;
     public SpriteRenderer sr;
+
+    private GameObject player;
+
+    public virtual void Awake()
+    {
+        //finds player gameobject
+        //player = GameObject.Find("Player");
+    }
+
     public virtual void OnTriggerEnter2D(Collider2D collision)
     {
         GameObject other = collision.gameObject;
@@ -15,10 +28,23 @@ public abstract class Interactable : MonoBehaviour
         //checks if collided with end of track and destroys gameobject if true
         else if (other.CompareTag("end"))
         {
-            Destroy(gameObject);
+            endOfTrack();
         }
     }
+    
+    public virtual void endOfTrack()
+    {
+        Destroy(gameObject);
+    }
+
     public abstract void PlayerInteraction();
+
+    public virtual void dealDmg()
+    {
+        /*
+         * player.takeDmg(damage);
+         */
+    }
 
     /*if needed
      * public abstract void Move();
