09bb20a54b16175a15f1a1559b105fd220876f1d
AdamPlett <72412169+AdamPlett@users.noreply.github.com>
2026-09-22 17:42:44 -0500

    More report card work and added text mesh pro

--- Assets/Scripts/UI/ReportCard.cs
+++ Assets/Scripts/UI/ReportCard.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using TMPro;
 
 public abstract class ReportCard : ScriptableObject
 {
@@ -13,6 +14,38 @@ public abstract class ReportCard : ScriptableObject
     public float perfectHits;
     [Tooltip("The number of bad hits the player performed"), Min(0)]
     public float badHits;
+    [Tooltip("The number of missed notes")]
+    public float missedHits;
     [Tooltip("The players health"), Min(0)]
     public float health;
+
+    [Header("UI")]
+    [SerializeField] private TMP_Text scoreText, pHitsText, bHitsText, mHitsText, hpText;
+
+    public virtual void SetScore(float lvlScore)
+    {
+        score = lvlScore;
+        scoreText.text = "Score: "+score+"/"+maxScore;
+    }
+    public virtual void SetPerfectHits(float pHits)
+    {
+        perfectHits = pHits;
+        pHitsText.text = "Perfect hits: "+perfectHits+"/"+maxHits;
+    }
+    public virtual void SetBadHits(float bHits)
+    {
+        badHits = bHits;
+        bHitsText.text = "Bad hits: " + badHits;
+    }
+    public virtual void SetMissedHits(float mHits)
+    {
+        missedHits = mHits;
+        mHitsText.text = "Missed hits:" + missedHits;
+    }
+    public virtual void SetHealth(float hp)
+    {
+        health = hp;
+        hpText.text = "Health: ";
+        //logic for showing # of hearts
+    }
 }
