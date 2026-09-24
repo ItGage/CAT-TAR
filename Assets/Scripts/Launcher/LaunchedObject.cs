541f31de6d8f9f7b3db5f02b6a634d94dd7a10d2
Ryan <rak88@txstate.edu>
2026-09-23 22:45:34 -0500

    Launcher now moves things!
    Launcher now controls movement

---
+++ Assets/Scripts/Launcher/LaunchedObject.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class LaunchedObject
+{
+    public Interactable interactable;
+    public Vector2 startPosition;
+    public Vector2 targetPosition;
+    public int spawnSixteenth;
+    public int targetSixteenth;
+}
---
+++ Assets/Scripts/Launcher/Launcher.cs
@@ -0,0 +1,116 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Launcher : MonoBehaviour
+{
+    public SongChart songChart;
+
+    public Transform[] spawnPoints;
+    public Transform[] targetPoints;
+
+    public int leadInSixteenths = 8;
+   
+    private int nextSixteenthToLaunch = 8;
+
+    private List<LaunchedObject> activeObjects = new();
+
+    void Update()
+    {
+        int launchAtSixteenth = nextSixteenthToLaunch - leadInSixteenths;
+
+        //----------------------------------------Spawning Section------------------------------------//
+
+        if (Conductor.instance.songPositionInSixteenths >= launchAtSixteenth)
+        {
+
+            int measureIndex = nextSixteenthToLaunch / 16;
+            int sixteenthIndex = nextSixteenthToLaunch % 16;
+
+            //keeps you from going past the end of song
+            if (measureIndex < songChart.measures.Count)
+            {
+                //grabs current measure and stores locally
+                Measure currentMeasure = songChart.measures[measureIndex];
+
+                //loops through each lane
+                for (int laneIndex = 0; laneIndex < currentMeasure.lanes.Length; laneIndex++)
+                {
+                    //grabs current sixteenth index and stores locally as "slot"
+                    Sixteenth slot = currentMeasure.lanes[laneIndex].sixteenths[sixteenthIndex];
+
+                    //interactable testing
+                    Debug.Log
+                    (
+                        "Checking lane " + laneIndex +
+                        ", measure " + measureIndex +
+                        ", sixteenth " + sixteenthIndex +
+                        ", interactable = " +
+                        (slot.interactable == null ? "NULL" : slot.interactable.name)
+                    );
+
+                    //if there is an interactable on this slot, spawn it
+                    if (slot.interactable != null)
+                    {
+                        Interactable spawnedObject = Instantiate
+                        (
+                            slot.interactable,
+                            spawnPoints[laneIndex].position,
+                            spawnPoints[laneIndex].rotation
+                        );
+
+                        Debug.Log
+                        (
+                            "Spawned " + spawnedObject.name +
+                            " in lane " + laneIndex
+                        );
+
+                        if (spawnedObject.isMoveable)
+                        {
+                            LaunchedObject launchedObject = new()
+                            {
+                                interactable = spawnedObject,
+                                startPosition = spawnPoints[laneIndex].position,
+                                targetPosition = targetPoints[laneIndex].position,
+                                spawnSixteenth = nextSixteenthToLaunch - leadInSixteenths,
+                                targetSixteenth = nextSixteenthToLaunch
+                            };
+
+                            activeObjects.Add(launchedObject);
+                        }
+                    }
+                }
+
+                Debug.Log(Conductor.instance.totalSixteenth);
+                Debug.Log("Ready to launch sixteenth: " + nextSixteenthToLaunch);
+                Debug.Log
+                (
+                    "Global: " + nextSixteenthToLaunch +
+                    " Measure: " + measureIndex +
+                    " Sixteenth: " + sixteenthIndex
+                );
+
+                nextSixteenthToLaunch++;
+            }
+        }
+
+        //----------------------------------------Movement Section------------------------------------//
+
+        for (int i = 0; i < activeObjects.Count; i++)
+        {
+            LaunchedObject launchedObject = activeObjects[i];
+
+            float progress =
+                (Conductor.instance.songPositionInSixteenths - launchedObject.spawnSixteenth) /
+                (launchedObject.targetSixteenth - launchedObject.spawnSixteenth);
+
+            Vector2 newPosition = Vector2.Lerp
+            (
+                launchedObject.startPosition,
+                launchedObject.targetPosition,
+                progress
+            );
+
+            launchedObject.interactable.transform.position = newPosition;
+        }
+    }
+}
