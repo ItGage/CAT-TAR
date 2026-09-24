5cb87768501c27c6f32ae2ba6edc872bcf967770
ItGage <Gdelosh18@gmail.com>
2026-09-24 12:17:35 -0500

    Created Attack Meter Script & Added Edge Collider

--- Assets/Scripts/Interactables/AttackMeter.cs
+++ Assets/Scripts/Interactables/AttackMeter.cs
@@ -2,33 +2,47 @@ using UnityEngine;
 
 public class AttackMeter : MonoBehaviour
 {
-    public float duration, scoreNeeded, damage=3;
+    public float duration;
+
+    [Header("Score")]
+    [SerializeField] private float targetScore;
+    [SerializeField] private float maxScore;
+    [Space(5)]
+    [SerializeField] private float currentScore;
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+[Header("Note Increments")]
+    [SerializeField] private float baseIncrement;
+    [SerializeField] private float badMultiplier;
+    [SerializeField] private float goodMultiplier;
+    [SerializeField] private float perfectMultiplier;
 
     public void BadHit()
     {
-
+        FillMeter(baseIncrement * badMultiplier);
     }
 
     public void GoodHit()
     {
-
+        FillMeter(baseIncrement * goodMultiplier);
     }
 
     public void PerfectHit()
     {
+        FillMeter(baseIncrement * perfectMultiplier);
+    }
 
+    private void FillMeter(float amount)
+    {
+        float newScore = currentScore + amount;
+
+        if(newScore < maxScore)
+        {
+            currentScore = newScore;
+        }
+        else
+        {
+            currentScore = maxScore;
+        }
     }
 }
