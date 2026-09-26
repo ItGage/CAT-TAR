4b9ef22c8d86fc0903653fd03432b2c21d47eac1
ItGage <Gdelosh18@gmail.com>
2026-09-25 19:33:06 -0500

    Interactables now Scale

--- Assets/Scripts/Launcher/LaunchedObject.cs
+++ Assets/Scripts/Launcher/LaunchedObject.cs
@@ -3,8 +3,13 @@ using UnityEngine;
 public class LaunchedObject
 {
     public Interactable interactable;
+    [Space(5)]
     public Vector2 startPosition;
     public Vector2 targetPosition;
+    [Space(5)]
+    public Vector2 startScale;
+    public Vector2 targetScale;
+    [Space(5)]
     public int spawnSixteenth;
     public int targetSixteenth;
 }
